accaf1d314390e1276ac26302f25384a152be05a
Trevor Hein <trevor-hein@uiowa.edu>
2014-03-25 10:16:03 -0500

    Initial Commit New Files

---
+++ dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[reID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [reName] NVARCHAR(50) NOT NULL, 
+    [reSubmit] NVARCHAR(50) NOT NULL, 
+    [reIngrdient1] NVARCHAR(50) NOT NULL, 
+    [reIngredient2] NVARCHAR(50) NULL, 
+    [reIngredient3] NVARCHAR(50) NULL, 
+    [reIngredient4] NVARCHAR(50) NULL, 
+    [reIngredient5] NVARCHAR(50) NULL, 
+    [rePreperation] NVARCHAR(MAX) NOT NULL, 
+    [reNotes] NVARCHAR(MAX) NULL
+)
